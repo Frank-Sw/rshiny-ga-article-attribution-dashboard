ddfb59b4870dd25b5a0e436cb06f163f1447616c
Justin Marciszewski <justin@empiricalpath.com>
2017-08-09 12:03:47 -0400

    added missing parentheses

--- dashboards/extract.R
+++ dashboards/extract.R
@@ -4,7 +4,7 @@ library(googleCloudStorageR)
 
 ## set options
 gcs_global_bucket("rshiny-ga-article-dash")
-options(googleAuthR.scopes.selected = c("https://www.googleapis.com/auth/cloud-platform")
+options(googleAuthR.scopes.selected = c("https://www.googleapis.com/auth/cloud-platform"))
 
 ## auth on the VM
 gar_gce_auth()
