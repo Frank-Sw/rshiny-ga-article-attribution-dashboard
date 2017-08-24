e3ed076f2578bbbc719ca48e997570ded99114c5
Justin Marciszewski <justin@empiricalpath.com>
2017-08-24 13:46:38 -0400

    added second list for demo purposes

--- dashboards/config.R
+++ dashboards/config.R
@@ -15,3 +15,20 @@ lookup12345678 <- c(date = "date",
                   transactions = "transactions",
                   transactionRevenue = "revenue",
                   uniquePageViews = "uniquePageviews")
+
+## GA View 1111111 
+## use this as a template, copy/paste for each GA view
+## then change indicies of custom dimensions 
+lookup1111111 <- c(date = "date",
+                  dimension00 = "hitArticlePublishDate",
+                  dimension00 = "hitArticleTitle",
+                  dimension00 = "hitArticleAuthor",
+                  dimension00 = "sessionFirstArticlePublishDate",
+                  dimension00 = "sessionFirstArticleTitle",
+                  dimension18 = "sessionFirstArticleAuthor",
+                  goal00Completions = "events",
+                  goal00ConversionRate = "eventConversionRate",
+                  sessions = "sessions",
+                  transactions = "transactions",
+                  transactionRevenue = "revenue",
+                  uniquePageViews = "uniquePageviews")
